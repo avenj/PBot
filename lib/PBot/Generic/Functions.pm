987ccbdbd683eb7a352d576b4586edd59caac72b
Matthew Barksdale <mattwb65@gmail.com>
2013-05-14 05:40:40 +0000

    Generic functions (message, notice, etc)

---
+++ lib/PBot/Generic/Functions.pm
@@ -0,0 +1,16 @@
+package PBot::Generic::Functions;
+
+use strict;
+use warnings;
+
+sub message
+{
+    my ($self, $data) = @_;
+    $self->network->write("PRIVMSG ".$self->name." :$data");
+}
+
+sub notice
+{
+    my ($self, $data) = @_;
+    $self->network->write("NOTICE ".$self->name." :$data");
+}   
