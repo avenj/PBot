5da1fd2e115d232a630077aeee7efda50be3a115
Matthew Barksdale <mattwb65@gmail.com>
2013-05-14 03:46:10 +0000

    Add 'loop' instance field and 'start' method

--- lib/PBot.pm
+++ lib/PBot.pm
@@ -6,11 +6,28 @@ use strict;
 use warnings;
 use Moo;
 
+use IO::Async;
+use IO::Async::Loop;
+
+use PBot::Connection;
 use PBot::Network;
 
 has debug => (
     is      => 'ro',
     default => 0
 );
+
+has loop => (
+    is      => 'ro',
+    default => sub { IO::Async::Loop->new; }
+);
+
+sub start {
+    my $self = shift;
+    say "*** Starting PBot";
+    say "*** Debug on" if $self->debug;
+    say "*** Entering loop...";
+    $self->loop->run;
+}
 
 1;
