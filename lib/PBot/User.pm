54562503330a5be3838e35c3914351eba8ec9a0b
Matthew Barksdale <mattwb65@gmail.com>
2013-05-14 03:16:24 +0000

    Start of PBot::User

---
+++ lib/PBot/User.pm
@@ -0,0 +1,53 @@
+package PBot::User;
+
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+
+has nick => (
+    is       => 'rw',
+    isa      => Str,
+    required => 1
+);
+
+has user => (
+    is       => 'rw',
+    isa      => Str, 
+    required => 1
+);
+
+has host => (
+    is       => 'rw',
+    isa      => Str,
+    required => 1
+);
+
+has channels => (
+    is       => 'rw',
+    isa      => ArrayRef[InstanceOf['PBot::Channel']],
+    default  => [ ],
+    weak_ref => 1
+);
+
+sub in_channel
+{
+    my ($self, $channelName) = @_;
+    foreach (@{$self->channels})
+    {
+        return 1 if lc($channelName) eq lc($_->name);
+    }
+    return undef;
+}
+
+sub add_to_channel
+{
+    my ($self, $channelObj) = @_;
+    return if !ref $channelObj || blessed($channelObj) ne 'PBot::Channel';
+    push(@{$self->channels}, $channelObj);
+}
+
+sub del_from_channel
+{
+    my ($self, $channelObj) = @_;
+    return if !ref $channelObj || blessed($channelObj) ne 'PBot::Channel';
+    @{$self->channels} = grep { $channelObj != $_ } @{$self->channels};
+}
